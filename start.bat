1e7c84a723c0f134650181a199ee2512f5825f68
Monish B <31185862+bmonish@users.noreply.github.com>
2020-06-26 20:57:27 +0530

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,11 @@
+python main.py
+Good evening to one and all! 
+I extend my warm welcome to my faculties, principal, viceprincipals and my dearest alumni.
+"No matter how much you hate college, you will miss it when you leave."
+I hope this everyone would have heard this quote atleast once in their college life.
+But college friendship also turn out to be the best time of our life and the friends we get in college become our bestest.
+Amidst the hustle and bustle of the modern world taking time out of our lives to connect with our friends is becoming a tedious task.
+But alumni meetings like this bring you one step closer to the process and takes you backward in time to the golden days.
+
+I hope everyone, by at the end of the session will have relived the golden days with lots of laughter, joy and surprises and if possible happy tears.
+
